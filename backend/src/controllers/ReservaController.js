04de937f6c6866ce201c5dcf1734a3274f76e9e4
jotapgn <joaopedrogomes26@gmail.com>
2020-05-04 20:32:31 -0300

    Add Yup

--- backend/src/controllers/ReservaController.js
+++ backend/src/controllers/ReservaController.js
@@ -1,17 +1,39 @@
 import Reserva from '../models/Reserva';
 import Hotel from '../models/Hotel';
 import Usuario from '../models/Usuario';
+import * as Yup from 'yup';
 
 class ReservaController{
 
     async index(req, res){
+        const { responsavel } = req.body;
         let reservas = await Reserva.find({ responsavel });
-        return res.json(reservas)
+        return res.json(reservas);
     }
 
     async store(req, res) {
-        const { usuario_id, dataInicial, dataFinal, qtdeHospedes } = req.body;
+        const { usuario_id } = req.headers;
+        const { dataInicial, dataFinal, qtdeHospedes } = req.body;
         const { hotel_id } = req.params;
+        const schema = Yup.object().shape(
+            {
+                dataInicial = Yup.string().required(),
+                dataFinal = Yup.string().required(),
+                qtdeHospedes: Yup.number().required(),
+            }
+        );
+        if(! (await schema.isValid(req.body))){
+            return res.status(400).json({mensaem: 'Dados inválidos!'})
+        }
+
+        Usuario.findById(usuario_id).catch((err) =>{
+            return res.status(401).json({mensaem: 'Usuário não autorizado!'})
+        });
+
+        Hotel.findById(hotel_id).catch((err) =>{
+            return res.status(400).json({mensaem: 'Hotel inválido!'})
+        });
+
         let reserva = await Reserva.create({
             responsavel: usuario_id,
             hotel: hotel_id,
