25840c1984279f448c8cb8240ad9a05b8a5b6e57
jotapgn <joaopedrogomes26@gmail.com>
2020-05-11 14:52:02 -0300

    Add Yup Conditional Validation

--- backend/src/controllers/ReservaController.js
+++ backend/src/controllers/ReservaController.js
@@ -1,7 +1,7 @@
 import Reserva from '../models/Reserva';
 import Hotel from '../models/Hotel';
 import Usuario from '../models/Usuario';
-import * as Yup from 'yup';
+import * as yup from 'yup';
 
 class ReservaController{
 
@@ -15,11 +15,16 @@ class ReservaController{
         const { usuario_id } = req.headers;
         const { dataInicial, dataFinal, qtdeHospedes } = req.body;
         const { hotel_id } = req.params;
-        const schema = Yup.object().shape(
+        const schema = yup.object().shape(
             {
-                dataInicial = Yup.string().required(),
-                dataFinal = Yup.string().required(),
-                qtdeHospedes: Yup.number().required(),
+                dataInicial: yup
+                .string()
+                .required(),
+                dataFinal: yup.string()
+                .required(),
+                qtdeHospedes: yup.
+                number().
+                required(),
             }
         );
         if(! (await schema.isValid(req.body))){
