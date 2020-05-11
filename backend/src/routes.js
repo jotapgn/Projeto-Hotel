ed590293c0c7838f4ae4e2ea7673977c60e540ad
jotapgn <joaopedrogomes26@gmail.com>
2020-05-11 20:27:57 -0300

    Add Multer

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,15 +6,21 @@ import HotelController from './controllers/HotelController';
 
 import ReservaController from './controllers/ReservaController';
 
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+
 
 const routes = new Router();
+
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.get('/sessions', SessionController.index);
 //routes.put('/sessions', SessionController.update);
 
 routes.post('/hoteis', HotelController.store);
-routes.get('/hoteis', HotelController.index);
+routes.get('/hoteis', upload.single('imagem'), HotelController.index);
 
 
 routes.post('/hoteis/:hotel_id/reserva', ReservaController.store);
