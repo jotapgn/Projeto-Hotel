6141d57146e6a3d47f0c9675dd37b888348ce8a1
jotapgn <joaopedrogomes26@gmail.com>
2020-05-18 20:36:12 -0300

    Add new HotelSchema

--- backend/src/models/Hotel.js
+++ backend/src/models/Hotel.js
@@ -8,6 +8,14 @@ const HotelSchema = new Schema({
     qtdeAptos: Number,
     valorDiaria: Number,
     nomeImagem: String,
+},{
+    toJSON:{
+        virtuals:true,
+    }
+});
+
+HotelSchema.virtual('imagem_url').get(function(){
+    return `http://localhost:3000/uploads/${this.nomeImagem}`;
 });
 
 export default model('Hotel', HotelSchema);
